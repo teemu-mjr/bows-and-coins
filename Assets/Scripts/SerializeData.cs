fc49241360b22c456a0e3133c1f65be0d6b85819
teemu-mjr <teemu.ma.rintala@outlook.com>
2021-10-29 10:13:42 +0300

    Serializeing with xml

--- Assets/Scripts/SerializeData.cs
+++ Assets/Scripts/SerializeData.cs
@@ -7,9 +7,6 @@ public class SerializeData
 {
     private static readonly string SAVE_FOLDER = Application.dataPath + "/Saves/";
 
-    //private static string userFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-    //private static string fileLocation = @$"{userFolderPath}\Bows and Coins\";
-
 
     public SerializeData()
     {
@@ -23,7 +20,7 @@ public class SerializeData
     {
         XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
 
-        using(TextWriter writer = new StreamWriter(@$"{SAVE_FOLDER}\playerStats.json"))
+        using(TextWriter writer = new StreamWriter(@$"{SAVE_FOLDER}\playerStats.xml"))
         {
             xmlSerializer.Serialize(writer, data);
         }
@@ -33,9 +30,9 @@ public class SerializeData
     public T LoadData<T>()
     {
         XmlSerializer deserializer = new XmlSerializer(typeof(T));
-        if (File.Exists(@$"{SAVE_FOLDER}\playerStats.json"))
+        if (File.Exists(@$"{SAVE_FOLDER}\playerStats.xml"))
         {
-            TextReader reader = new StreamReader(@$"{SAVE_FOLDER}\playerStats.json");
+            TextReader reader = new StreamReader(@$"{SAVE_FOLDER}\playerStats.xml");
             object obj = deserializer.Deserialize(reader);
             reader.Close();
             return (T)obj;
